338262ce6349629b019fa3f1549964e75ef22b22
Jonathan Rivers <jrivers@paradigm4.com>
2016-01-21 15:59:10 -0500

    added a simple test

--- src/test.sh
+++ src/test.sh
@@ -8,20 +8,17 @@ iquery -anq "store( build( <val:string> [x=0:255,10,0],  string(x % 256) ), zero
 iquery -anq "store( build( <val:string> [x=0:2000000,10000,0],  string(x % 256) ), big )"                       > /dev/null 2>&1
 
 
-iquery -aq "fast_count(zero_to_255)"
-> /dev/null 2>&1
+rm test.out
+rm test.expected
+
+touch ./test.expected
 
 
+iquery -o csv -aq "fast_count(zero_to_255)" >> test.out
 
-rm -rf /tmp/fastcount_test
-mkdir /tmp/fastcount_test
-touch /tmp/fastcount_test/test.expected
-
-
-iquery -aq "fast_count(zero_to_255)" >> test.out
-
-echo '255
-' >> /tmp/fastcount_test/test.expected
+echo 'count' >> ./test.expected
+echo '256
+' >> ./test.expected
 
 
 
