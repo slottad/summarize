73259ebc2d69b1f19a3fa9f0bff7ffce696d4db4
Jonathan Rivers <jrivers@paradigm4.com>
2016-01-22 11:04:52 -0500

    need the hash for now, add this back

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,17 +1,3 @@
-/*
- **
- * BEGIN_COPYRIGHT
- *
- * PARADIGM4 INC.
- * This file is part of the Paradigm4 Enterprise SciDB distribution kit
- * and may only be used with a valid Paradigm4 contract and in accord
- * with the terms and conditions specified by that contract.
- *
- * Copyright (C) 2010 - 2015 Paradigm4 Inc.
- * All Rights Reserved.
- *
- * END_COPYRIGHT
- */
 
 #include <vector>
 
