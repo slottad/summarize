e0d55a6e7818b297bc0473d9bc640f9ff31603a1
Jonathan Rivers <jrivers@paradigm4.com>
2016-01-22 10:53:33 -0500

    updated tests

--- src/test.sh
+++ src/test.sh
@@ -2,8 +2,9 @@
 
 iquery -anq "remove(zero_to_255)"                > /dev/null 2>&1
 
-iquery -anq "store( build( <val:string> [x=0:255,10,0],  string(x % 256) ), zero_to_255 )"                       > /dev/null 2>&1
+iquery -anq "store( build( <val:string> [x=0:255,10,0],  string(x % 256) ), zero_to_255 )"  > /dev/null 2>&1
 
+iquery -anq "store( build( <val:string> [x=0:255,10,5],  string(x % 256) ), zero_to_255_overlap )"  > /dev/null 2>&1
 
 rm test.out
 rm test.expected
@@ -16,6 +17,16 @@ iquery -o csv -aq "fast_count(zero_to_255)" >> test.out
 echo 'count' >> ./test.expected
 echo '256
 ' >> ./test.expected
+
+iquery -o csv -aq "fast_count(between(zero_to_255,0,9))" >> test.out
+
+echo 'count' >> ./test.expected
+echo '10' >> ./test.expected
+
+iquery -o csv -aq "fast_count(zero_to_255_overlap)" >> test.out
+echo 'count' >> ./test.expected
+echo '256' >> ./test.expected
+
 
 
 
